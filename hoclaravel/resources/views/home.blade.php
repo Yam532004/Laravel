988594b92121cd1cede2613fbcb09446c6743df3
Yam532004 <yam532004@gmail.com>
2024-02-03 00:20:43 +0700

    Laravel-Bai5

--- hoclaravel/resources/views/home.blade.php
+++ hoclaravel/resources/views/home.blade.php
@@ -1,5 +1,3 @@
 <h1 style="text-align: center;"> Hoc lap trinh laravel</h1>
-<?php
-echo date('Y-m-d H:i:s');
-echo config('app.env');
-?>
+<!-- Trong id va slug co the dung vong lap de lay id va slug cua tung URL trong database -->
+<a href="<?php echo route('admin.tintuc', ['id'=>1, 'slug'=>'tin-tuc-the-gioi'])?>">xem tin tuc</a>
