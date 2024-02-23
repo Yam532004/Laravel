a5b6012de0ae5de607e2afe7ff290c54b4a5c0c7
Yam532004 <yam532004@gmail.com>
2024-02-23 09:40:57 +0700

    Learned about for loop and if-else...

--- hoclaravel/resources/views/home.blade.php
+++ hoclaravel/resources/views/home.blade.php
@@ -1,21 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Unicode - Hoc lap trinh web bai ban </title>
-</head>
-<body>
-    <header>
-        <h1>HEADER - UNICODE</h1>
-        <h2>{{$title}}</h2>
-    </header>
-    <main>
-        <h1>NOI DUNG - UNICODE</h1>
-        <h2>{{$content}}</h2>
-    </main>
-    <header>
-        <h1>FOOTER - UNICODE</h1>
-    </header>
-</body>
-</html>
+<h1>Homepage UNICODE</h1>
+<h2>{{!empty(request()->keyword)?request()->keyword:'Nothing'}}</h2>
+<div class="container">
+    {!! !empty($content)?$content:false!!}
+</div>
+<hr>
+
+<!-- @for ($i=1; $i<=10; $i++)
+    <p> Item: {{$i}}</p>
+@endfor -->
+
+<!-- @while ($i<=10)
+   <p>Item: {{$i}}</p>
+   @php 
+    $i++;
+   @endphp
+@endwhile -->
+
+<!-- 
+@foreach ($dataArr as $key => $item)
+    <p>This is {{ $item }} - {{$key}}</p>
+@endforeach -->
+
+<!-- @forelse ($dataArr as $item)
+    <li>{{$item}}</li>
+@empty
+    <p>No item</p>
+@endforelse -->
+
+<!-- @if ($number<=0) 
+    <p>Bad</p>
+    @elseif ($number>0 && $number<5) 
+        <p>This is small number</p>
+    @elseif ($number>5 && $number<10)
+        <p>This is arrage number</p>
+    @else
+    <p>Big Number</p>
+@endif -->
+
+<!-- @switch($number)
+    @case(1)
+    @case(3)
+    @case(5)
+        First case...
+        @break
+ 
+    @case(2)
+        Second case...
+        @break
+ 
+    @default
+        Default case...
+@endswitch -->
+
+@for ($i=1; $i<=10; $i++)
+    @if ($i==5) 
+        @continue
+    @endif 
+        <p> The Item: {{$i}}</p>
+        @endfor
