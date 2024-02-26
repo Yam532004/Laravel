8cd70da2fdf1698ce33ac63ff6a44b6b063f875d
Yam532004 <yam532004@gmail.com>
2024-02-26 23:41:28 +0700

    Laravel-Bai12

--- hoclaravel/resources/views/home.blade.php
+++ hoclaravel/resources/views/home.blade.php
@@ -4,57 +4,10 @@
     {!! !empty($content)?$content:false!!}
 </div>
 <hr>
+@php
+// $message = 'Order is successful'
+@endphp
 
-<!-- @for ($i=1; $i<=10; $i++)
-    <p> Item: {{$i}}</p>
-@endfor -->
+@include('parts.notice')
 
-<!-- @while ($i<=10)
-   <p>Item: {{$i}}</p>
-   @php 
-    $i++;
-   @endphp
-@endwhile -->
 
-<!-- 
-@foreach ($dataArr as $key => $item)
-    <p>This is {{ $item }} - {{$key}}</p>
-@endforeach -->
-
-<!-- @forelse ($dataArr as $item)
-    <li>{{$item}}</li>
-@empty
-    <p>No item</p>
-@endforelse -->
-
-<!-- @if ($number<=0) 
-    <p>Bad</p>
-    @elseif ($number>0 && $number<5) 
-        <p>This is small number</p>
-    @elseif ($number>5 && $number<10)
-        <p>This is arrage number</p>
-    @else
-    <p>Big Number</p>
-@endif -->
-
-<!-- @switch($number)
-    @case(1)
-    @case(3)
-    @case(5)
-        First case...
-        @break
- 
-    @case(2)
-        Second case...
-        @break
- 
-    @default
-        Default case...
-@endswitch -->
-
-@for ($i=1; $i<=10; $i++)
-    @if ($i==5) 
-        @continue
-    @endif 
-        <p> The Item: {{$i}}</p>
-        @endfor
---
+++ hoclaravel/resources/views/parts/notice.blade.php
@@ -0,0 +1,3 @@
+@if (!empty($message))
+<h2>{{$message}}</h2>
+@endif
