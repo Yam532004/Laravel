328154033b970a8d55fab8faa963a2dad8f051ed
Yam532004 <yam532004@gmail.com>
2024-03-02 15:17:38 +0700

    Laravel-Bai18

--- hoclaravel/routes/web.php
+++ hoclaravel/routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\Admin\ProductsController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\HomeController;
-
+use Illuminate\Http\Response;
 
 
 
@@ -22,6 +22,7 @@ use App\Http\Controllers\HomeController;
 |
 */
 // Client route
+
 Route::middleware('auth.admin')->prefix('categories')->group(function () {
     // danh sach chuyen muc
     Route::get('/', [CategoryController::class, 'index'])->name('categories.list');
@@ -43,16 +44,15 @@ Route::middleware('auth.admin')->prefix('categories')->group(function () {
 
     //Hien thi form upload
     Route::get('/upload', [CategoryController::class, 'getFile']);
-    
+
     //Xu ly file
     Route::post('/upload', [CategoryController::class, 'handleFile'])->name('category.upload');
-
 });
 
 Route::get('san-pham/{id}', [HomeController::class, 'getProductDetail']);
 // Admin route
 Route::middleware('auth.admin')->prefix('admin')->group(function () {
-    Route::get('/',[DashboardController::class, 'index']);
+    Route::get('/', [DashboardController::class, 'index']);
     Route::resource('products', ProductsController::class)->middleware('auth.admin.product');
 });
 
@@ -63,6 +63,36 @@ Route::get('add-product', [HomeController::class, 'getAdd']);
 // Route::post('add-product', [HomeController::class, 'postAdd']);
 Route::put('add-product', [HomeController::class, 'postAdd']);
 
+Route::get('get-inf', [HomeController::class, 'getArr']);
 
+// Response
+Route::get('demo-response', function () {
+    // $response = new Response("Hoc lap trinh tai Unicode", 200);
+    // $response = response('Hoc lap trinh tai Unicodde', 404);
+    // $content = '<h2>Hoc lap trinh tai Unicode </h2>';
+    // $content = json_encode ([
+    //     'Item 1',
+    //     'Item 2',
+    //     "Item 3"
+    // ]);
+    // $response = (new Response($content))->header('content-Type', 'application/json');
 
+    $response = (new Response())->cookie('unicode', 'Trainning PHP - Laravel', 30);
+    return $response;
+});
 
+// Cookie
+Route::get('demo-response-cookie', function (Request $request) {
+    return $request->cookie('unicode');
+});
+
+Route::get('demo-response-view', function () {
+    // return view('clients.demo-test');
+    $response = response()
+        ->view('clients.demo-test', [
+            'title' => 'Hoc HTTP response'
+        ], 201)
+        ->header('Content-Type', 'application/json')
+        ->header('API-Key', '123456');
+    return $response;
+});
